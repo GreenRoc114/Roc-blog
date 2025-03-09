ccc6619888982228f1a1c7608b97b0e1ee840a57
GreenRoc114 <168276820+GreenRoc114@users.noreply.github.com>
2025-03-09 15:31:18 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -7,8 +7,8 @@ import type {
 import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
-  title: '熔岩墨迹',
-  subtitle: 'Magma Ink',
+  title: 'Ahhf45',
+  subtitle: 'GreenRoc的博客',
   lang: 'zh_CN',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
     hue: 50,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
@@ -60,8 +60,8 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
   avatar: 'assets/images/avatar.jpg',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: '岩浆块Magma',
-  bio: '为美好的生活献礼',
+  name: 'GreenRoc',
+  bio: 'There is no fate but what we make for ourselves.',
   links: [
     {
       name: "GitHub",
