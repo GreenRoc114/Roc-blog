2b55ffc9ec923ab0351bb5ecdb2ef2067854d429
GreenRoc114 <168276820+GreenRoc114@users.noreply.github.com>
2025-03-10 21:58:18 +0800

    Update LinkGridList.vue

--- src/components/links/LinkGridList.vue
+++ src/components/links/LinkGridList.vue
@@ -61,7 +61,7 @@ const links: { title: string; link: string; desc: string; img: string }[] = [
   {
     link: "http://linearteam.top",
     title: "线形团队官网",
-    img: "http://linearteam.top/wp-content/uploads/2025/03/logo-2.png",
+    img: "linearteam.top/wp-content/uploads/2025/03/logo-4.png",
     desc: "线形团队LinearTeam",
   },
   
